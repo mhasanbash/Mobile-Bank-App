8c1ba3e0b89d279c55256af19933ad1f6e4e0840
hasan <99391294+mhasanbash@users.noreply.github.com>
2024-01-28 23:27:06 +0330

    complete athentication

--- bank/management/urls.py
+++ bank/management/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import Home, Profile, Signin
+from .views import Home, Profile, Signin, signout
 
 app_name = 'management'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('Home/', Home.as_view(), name='Home'),
     path('profile/', Profile.as_view(), name='profile'),
     path('signin/', Signin.as_view(), name='signin'),
+    path('signout/', signout, name='signout'),
 ]
--- bank/management/views.py
+++ bank/management/views.py
@@ -4,6 +4,11 @@ from .form import SignInForm
 from django.views import View
 from django.http import HttpResponse
 from django.db import connection
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.contrib.sessions.models import Session
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class Home(View):
     def get(self, request):
@@ -22,13 +27,16 @@ class Profile(View):
     def post(self, request):
         # کد برای پردازش درخواست POST
         return HttpResponse("This is a POST response")
-    
+
+
+
+#_________________________________________________________________________________________________ 
 class Signin(View):
     # username and password
     def get(self, request):
         form = SignInForm()
-        form.fields['username'].widget.attrs.update({'class': 'form-control'})  # اضافه کردن کلاس CSS به فیلد username
-        form.fields['password'].widget.attrs.update({'class': 'form-control'})  # اضافه کردن کلاس CSS به فیلد password
+        form.fields['username'].widget.attrs.update({'class': 'form-control'})
+        form.fields['password'].widget.attrs.update({'class': 'form-control'}) 
         return render(request, "signin.html", {'form': form})
     
     def post(self, request):
@@ -42,7 +50,17 @@ class Signin(View):
                 with connection.cursor() as cursor:
                     cursor.execute("SELECT * FROM USERS WHERE username = %s and password = %s", [username, password])
                     instances = cursor.fetchall()
-                    return HttpResponse(instances)
+                    if instances:
+                        request.session['username'] = instances[0][1]
+                        request.session['password'] = instances[0][2]
+                        request.session['first_name'] = instances[0][3]
+                        request.session['last_name'] = instances[0][4]
+                        request.session['address'] = instances[0][5]
+                        request.session['phonenumber'] = instances[0][6]
+                        request.session['email'] = instances[0][7]
+                        request.session['date_joined'] = instances[0][8]
+                        request.session['is_superuser'] = instances[0][9]
+                    return HttpResponseRedirect(reverse('management:Home'))
 
             except Exception as e:
                 return render(request, 'error.html', {'error_message': str(e)})
@@ -50,6 +68,14 @@ class Signin(View):
         else:
             return render(request, 'error.html', {'error_message': 'فرم ورود معتبر نیست'})
             # return render(request, 'home.html')
+
+
+def signout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('management:signin'))
+
+#________________________________________________________________________________________________________
+
 
 class changePassword(View):
     # user id and old_pass and new_pass
